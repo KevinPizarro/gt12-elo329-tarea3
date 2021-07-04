afcc77bec3589037db2079b1ea6018ea8d977679
Pablo_t <pablo.troncosop@sansano.usm.cl>
2021-07-04 18:59:34 -0400

    Se completa la clase comuna para la stage

--- Stage1/Comuna.cpp
+++ Stage1/Comuna.cpp
@@ -1,25 +1,58 @@
 #include "Comuna.h"
-Comuna::Comuna(double width, double length): territory(0,0,width,length){
-    pPerson=NULL;
+/**
+ * @brief Comuna::Comuna Se encarga de crear el objeto correspondiente a la persona, inicializa el tamaño de la comuna y se calcula el siguiente
+ * estado del individuo
+ * @param width Ancho de la comuna
+ * @param length Largo de la comuna
+ */
+Comuna::Comuna(double width, double length) : territory(0, 0, width, length) {
+    pPerson = NULL;
 }
+/**
+ * @brief Comuna::getWidth
+ * @return Ancho de la comuna
+ */
 double Comuna::getWidth() const {
-    return //.....
+    return territory.width();
 }
+/**
+ * @brief Comuna::getHeight
+ * @return Largo de la comuna
+ */
 double Comuna::getHeight() const {
-    return //.....
+    return territory.height();
 }
-void Comuna::setPerson(Pedestrian & person){
-   //....
+/**
+ * @brief Comuna::setPerson
+ * @param person Se inicializa la persona
+ */
+void Comuna::setPerson(Pedestrian& person) {
+    pPerson = &person;
 }
-void Comuna::computeNextState (double delta_t) {
-   //...
+/**
+ * @brief Comuna::computeNextState
+ * @param delta_t Variacion del tiempo entre cada calculo del estado
+ */
+void Comuna::computeNextState(double delta_t) {
+    pPerson->computeNextState(delta_t);
 }
-void Comuna::updateState () {
-   //....
+/**
+ * @brief Comuna::updateState Se actualiza la posición del individuo
+ */
+void Comuna::updateState() {
+    pPerson->updateState();
 }
-string Comuna::getStateDescription(){
-    return //....
+/**
+ * @brief Comuna::getStateDescription
+ * @return Se obtiene la descripcion del estado de la persona
+ */
+string Comuna::getStateDescription() {
+    return Pedestrian::getStateDescription();
 }
-string Comuna::getState() const{
-    return //...
+/**
+ * @brief Comuna::getState
+ * @return La posicion de la persona
+ */
+string Comuna::getState() const {
+    return pPerson->getState();
 }
