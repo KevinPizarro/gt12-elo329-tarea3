4818a9a16048ff893062403f4382f3b90d6675a8
Pablo_t <pablo.troncosop@sansano.usm.cl>
2021-07-06 22:09:08 -0400

    Se completa la clase pedestrian para la stage 2 comentado.

--- Stage2/Pedestrian.cpp
+++ Stage2/Pedestrian.cpp
@@ -1,7 +1,10 @@
 #include <QtMath> // for M_PI and functions, see https://doc.qt.io/qt-5/qtmath.html
 #include <string>
+#include <vector>
+#include <iostream>
 #include "Comuna.h"
 #include "Pedestrian.h"
+using namespace std;
 
 /**
  * @brief Pedestrian::Pedestrian: Clase de los individuos, contiene todos la informacion del individuo, asi como los metodos que utilizan al mismo
@@ -9,19 +12,14 @@
  * @param speed: velocidad del individuo
  * @param deltaAngle: Variacion maxima del angulo del individuo
  */
-Pedestrian::Pedestrian (Comuna &com, double speed, double deltaAngle): comuna(com) {
+Pedestrian::Pedestrian(Comuna *com, double speed, double deltaAngle): comuna(com) {
     myRand = QRandomGenerator::securelySeeded();
+    this->x = myRand.generateDouble()*com->getWidth();
+    this->y = myRand.generateDouble()*com->getHeight();
     this->speed = speed*(0.9+0.2*myRand.generateDouble());
     this->deltaAngle = deltaAngle;
-}
-/**
- * @brief Pedestrian::getState Metodo para obtener el estado actual del individuo
- * @return Retorna string con estado de la persona
- */
-string Pedestrian::getState() const {
-    string s=to_string(x) + ",\t";
-    s+= to_string(y);
-    return s;
+    this->state = S;
+    this->rec_time = 0;
 }
 /**
  * @brief Pedestrian::computeNextState: Metodo para calcular el estado del individuo
@@ -36,7 +34,7 @@ void Pedestrian::computeNextState(double delta_t) {
         angle = M_PI - angle;
         x_tPlusDelta=x+speed*qCos(angle)*delta_t;
         }
-    else if( x_tPlusDelta > comuna.getWidth()){ //rebound logic in x, right side
+    else if( x_tPlusDelta > comuna->getWidth()){ //rebound logic in x, right side
         angle = M_PI - angle;
         x_tPlusDelta=x+speed*qCos(angle)*delta_t;
     }
@@ -44,15 +42,53 @@ void Pedestrian::computeNextState(double delta_t) {
         angle = 2*M_PI - angle;
         y_tPlusDelta=y+speed*qSin(angle)*delta_t;
      }
-    else if( y_tPlusDelta > comuna.getHeight()){ //rebound logic in y, up side
+    else if( y_tPlusDelta > comuna->getHeight()){ //rebound logic in y, up side
         angle = 2*M_PI - angle;
         y_tPlusDelta=y+speed*qSin(angle)*delta_t;
+    }
+    if(state == I){
+        rec_time -= delta_t;
+        if(0 >= rec_time){
+            state = R;
+            rec_time = 0;
+        }
     }
 }
 /**
  * @brief Pedestrian::updateState: Metodo para actualizar la posicion del individuo
  */
 void Pedestrian::updateState(){
-    x=x_tPlusDelta;
-    y=y_tPlusDelta;
+    x = x_tPlusDelta;
+    y = y_tPlusDelta;
+}
+/**
+ * @brief Pedestrian::getState
+ * @return Retorna el estado de salud del  individuo
+ */
+State Pedestrian::getState(){
+    return state;
+}
+/**
+ * @brief Pedestrian::getX
+ * @return Retorna la posicion x del individuo
+ */
+double Pedestrian::getX(){
+    return x;
+}
+/**
+ * @brief Pedestrian::getY
+ * @return Retorna la posicion y del individuo
+ */
+double Pedestrian::getY(){
+    return y;
+}
+/**
+ * @brief Pedestrian::infect Metodo para infectar a un individuo susceptible
+ * @param rec Indica el tiempo de recuperacion del individuo.
+ */
+void Pedestrian::infect(double rec){
+    if(state == S){
+        state = I;
+    }
+    rec_time = rec;
 }
--- Stage2/Pedestrian.h
+++ Stage2/Pedestrian.h
@@ -2,23 +2,33 @@
 #define PEDESTRIAN_H
 #include <string>
 #include <QRandomGenerator>
+
 using namespace std;
+enum State{
+    S,I,R //S para susceptibles, I para infectados, R para recuperados
+};
 class Comuna;
-class Pedestrian {
+class Pedestrian{
 private:
     double x, y, speed, angle, deltaAngle;
     double x_tPlusDelta, y_tPlusDelta;
-    Comuna &comuna;
+    double rec_time;
+    State state;
+    Comuna *comuna;
     QRandomGenerator myRand; // see https://doc.qt.io/qt-5/qrandomgenerator.html
 
 public:
-    Pedestrian(Comuna &com, double speed, double deltaAngle);
+    Pedestrian(Comuna *com, double speed, double deltaAngle);
     static string getStateDescription() {
-        return "x, \ty";
+        return "Inf,\tRec,\tSus";
     };
-    string getState() const;
+    State getState();
     void computeNextState(double delta_t);
     void updateState();
+    double getX();
+    double getY();
+    void infect(double rec);
 };
+
 
 #endif // PEDESTRIAN_H
